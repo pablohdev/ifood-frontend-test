23c0488f8d842ac91e0203fe363dec4a823553a7
Pablo <desenvolvimento7@kurole.com>
2020-08-11 16:24:19 -0300

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 
-import light from './styles/themes/light';
+//import light from './styles/themes/light';
 import dark from './styles/themes/dark';
 
 import GlobalStyle from './styles/global';
